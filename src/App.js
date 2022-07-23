b4ae48664d7ba841b1e5ad87501dc8b82fbd00af
unknown <ezegonmac01@gmail.com>
2022-07-23 12:41:11 +0100

    Add basic api fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import { Component } from 'react';
 import './bootstrap.min.css';
 import Searcher from './components/Searcher';
 
-function App() {
-  return (
-    <div className="app container">
+class App extends Component {
+
+  state = {
+    term : ''
+  }
+
+  API_KEY = process.env.REACT_APP_API_KEY;
+    
+  fetchAPI = () => {
+    const perPage = 30;
+    const url = `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.state.term}&per_page=${perPage}`
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => console.log(res.hits))
+  }
+
+  searchData = (term) => {
+    // console.log(term);
+    this.setState({
+      term
+    }, () => {
+      this.fetchAPI()
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="app container">
       <div className="jumbotron">
         <p className="lead text-center">Image Searcher</p>
-        <Searcher/>
+        <Searcher 
+          message="Buscador..."
+          searchData={this.searchData} />
       </div>
+      {this.state.term}
     </div>
-  );
+    );
+}
+
 }
 
 export default App;
--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class Searcher extends Component {
-    state = {  } 
+
+    searchRef = createRef();
+
+    handleSearch = (e) => {
+        e.preventDefault();
+
+        // console.log(this.searchRef.current.value);
+
+        const term = this.searchRef.current.value;
+
+        // Send value to principal component
+        this.props.searchData(term);
+    }
+
     render() { 
         return (
-            <form>
+            <form onSubmit={this.handleSearch}>
                 <div className="row">
                     <div className="form-group col-md-11">
-                        <input type="text" className="form-control form-control-lg" placeholcer="Search your image. Example: Football"></input>
+                        <input ref={this.searchRef} type="text" className="form-control form-control-lg" placeholcer="Search your image. Example: Football"></input>
                     </div>
                     <div className="form-group col-md-1">
                         <input type="submit" className="btn btn-lg btn-danger btn-block" value="Search"></input>
